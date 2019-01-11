660372fbe7ae680371c889e900dcbc4b46d32008
suedar <690372513@qq.com>
2019-01-11 17:35:22 +0800

    api not settle yet

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -3,37 +3,48 @@
     <div class="head">
       <div class="left">
         <div class="image">
-            <img src="../assets/logo.jpg" alt="">
-          </div>
-          <!-- select返回的值设在status里面 -->
-          <el-select
-            v-model="value9"
-            multiple
-            filterable
-            remote
-            reserve-keyword
-            placeholder="请输入关键词"
-            :remote-method="remoteMethod"
-            :loading="loading">
-            <el-option
-              v-for="item in options4"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
-            </el-option>
-          </el-select>
+          <img src="../assets/logo.jpg" alt="">
+        </div>
+        <!-- select返回的值设在status里面 -->
+        <!-- <div class="search"> -->
+          <el-autocomplete
+            v-model="state4"
+            :fetch-suggestions="querySearchAsync"
+            placeholder="请输入内容"
+            @select="handleSelect"
+          >
+          <i
+            class="el-icon-search el-input__icon"
+            slot="suffix">
+          </i>
+          </el-autocomplete>
+          <!-- <input type="text"> -->
+        <!-- </div> -->
+          
       </div>
       
       <div class="tail">
         <template v-if="!isLogin">
-          <a href="http://">登陆</a>/
+          <a href="http://">登陆</a>
+          <span class="sperate_line">
+            |
+          </span>
           <a href="">注册</a>
         </template>
         <template v-else>
           {{name}}
         </template>
+        <span class="sperate_line">|</span>
+        <a href="">意见反馈</a>
       </div>
     </div>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-menu-item index="2">分类</el-menu-item>
+      <el-menu-item index="3">有声书物</el-menu-item>
+      <el-menu-item index="4">分享会</el-menu-item>
+      <el-menu-item index="5">榜单</el-menu-item>
+    </el-menu>
   </div>
 </template>
 
@@ -42,35 +53,40 @@ export default {
   name: 'mainPage',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      options4: [],
-      value9: [],
-      list: [],
-      loading: false,
-      states: [],
+      restaurants: [],
+      state4: '',
+      timeout:  null,
       isLogin: false,
-      name: ''
+      name: '',
+      activeIndex: '1'
     }
   },
   mounted() {
-      this.list = this.states.map(item => {
-        return { value: item, label: item };
-      });
-    },
+    this.restaurants = this.loadAll();
+  },
   methods: {
-    remoteMethod(query) {
-      if (query !== '') {
-        this.loading = true;
-        setTimeout(() => {
-          this.loading = false;
-          this.options4 = this.list.filter(item => {
-            return item.label.toLowerCase()
-              .indexOf(query.toLowerCase()) > -1;
-          });
-        }, 200);
-      } else {
-        this.options4 = [];
-      }
+    loadAll() {
+      return []
+    },
+    querySearchAsync(queryString, cb) {
+        var restaurants = this.restaurants;
+        var results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants;
+
+        clearTimeout(this.timeout);
+        this.timeout = setTimeout(() => {
+          cb(results);
+        }, 3000 * Math.random());
+    },
+    createStateFilter(queryString) {
+      return (state) => {
+        return (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+      };
+    },
+    handleSelect(item) {
+      console.log(item);
+    },
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
 }
@@ -83,25 +99,29 @@ export default {
   margin: 0 auto;
   .head {
     height: 100px;
-    line-height: 100px;
-    // display: flex;
-    // align-items: center;
-    // justify-content: center;
     .left{
       float: left;
       height: 100px;
-      // line-height: 100px;
-      // position: relative;
       .image{
+        height: 100px;
         img{
+          vertical-align: middle;
           height: 100px;
         }
         display: inline-block;
       }
-      // white-space: nowrap;
     }
     .tail {
       float: right;
+      font-size: 12px;
+      a {
+        color: #666;
+        text-decoration: none;
+        &:hover{
+          color: #aaa;
+        }
+
+      }
     }
   }
 }
